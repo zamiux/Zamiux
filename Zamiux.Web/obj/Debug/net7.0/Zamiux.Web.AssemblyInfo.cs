6d4bdae5329c5bb999f94c1fbabe302d1d8bbf8d
mohsen zamani <mohsen.1408@gmail.com>
2024-04-26 01:04:12 +0330

    Final

--- Zamiux.Web/obj/Debug/net7.0/Zamiux.Web.AssemblyInfo.cs
+++ Zamiux.Web/obj/Debug/net7.0/Zamiux.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zamiux.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8307dfe1fbcd09c18883b4f857506021fe5fcf20")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zamiux.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zamiux.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
